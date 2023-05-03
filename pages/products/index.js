68898182c76719a3dee09fa03b800c83d9f48c5d
dewsiro000 <norawit1267@gmail.com>
2023-05-03 23:21:19 +0700

    Nextjs-Tutorial

--- pages/products/index.js
+++ pages/products/index.js
@@ -3,27 +3,33 @@ import Image from "next/image"
 import styles from "@/styles/Product.module.css"
 import Link from "next/link"
 
-export async function getStaticProps(){
-    const res = await fetch("https://dummyjson.com/products?limit=12")
-    const data = await res.json()
-    return{
-        props:{products:data.products}
+//ดึงข้อมูลจาก Api 
+
+
+//  => async รอ ข้อมูลมาครบก่อนจึงไปทำงานที่ตัว Component
+export async function getStaticProps() {
+    const res = await fetch("https://dummyjson.com/products?limit=12")  // ?limit=12 ต้องการสินค้า 12 รายการ
+    const data = await res.json() // => ทำให้ข้อมูลที่ไป response มา เป็นรูปแบบของ json
+    return {  // ชื่อ props ตั้งชื่อว่า porduct    
+        props: { products: data.products }
     }
 }
 
-export default function Index({products}){
-    return(
+// นำ props ที่ไปดึงมาจาก Api ไปแสดงบน Component
+
+export default function Index({ products }) {
+    return (
         <>
             <Head>
                 <title>สินค้าทั้งหมด | KongRuksiam</title>
-                <meta name="keywords" content="kongruksiam,ร้านค้า,ขายเสื้อผ้า"/>
+                <meta name="keywords" content="kongruksiam,ร้านค้า,ขายเสื้อผ้า" />
             </Head>
             <div className={styles.container}>
-                {products.map(item=>(
+                {products.map(item => (
                     <div key={item.id}>
-                        <Link href={'/products/'+item.id}>
+                        <Link href={'/products/' + item.id}>
                             <h2 className={styles.title}>{item.title}</h2>
-                            <Image src={item.thumbnail} width={300} height={300} alt={item.title}/>
+                            <Image src={item.thumbnail} width={300} height={300} alt={item.title} />
                         </Link>
                     </div>
                 ))}
